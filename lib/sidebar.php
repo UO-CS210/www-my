5313721da4d5c6f0ae1aff6055d2f5e249b5d0c1
Michal Young <michal.young@gmail.com>
2025-07-08 16:16:57 -0700

    Trying to make the text line up.  Failing.

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -2,7 +2,7 @@
      Note links must be relative to site URL-->
 <div class="sidenav">
     <h1>Fall 2025</h1>
-  <h2>CS 210</h2>
-  <h3>UH 10-11:20</h3>
-  <h3>221 Allen</h3>
+    <h2>CS 210</h2>
+    <h3>UH 10-11:20</h3>
+    <h3>221 Allen</h3>
 </div> 
